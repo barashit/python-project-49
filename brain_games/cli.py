c12f755d91a67c5618ceec0af78b1ae59656a2a0
barashit <prostopesna74@gmail.com>
2024-09-19 18:16:22 +0300

    add flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,4 +4,3 @@ def welcome_user():
     print("Welcome to the Brain Games!")
     name = input("May I have your name? ")
     print(f"Hello, {name}!")
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,9 +3,11 @@
 
 from brain_games.cli import welcome_user
 
+
 def main():
+
     welcome_user()
+
 
 if __name__ == "__main__":
     main()
-
