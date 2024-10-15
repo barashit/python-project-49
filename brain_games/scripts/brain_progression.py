ab259e349ee34f719973a107c452e1be07a4e04a
barashit <prostopesna74@gmail.com>
2024-10-15 19:01:53 +0300

    Fixed brain-progression file-3

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -10,16 +10,16 @@ GAME_RULES = 'Answer "yes" if the number is prime, otherwise answer "no".'
 def generate_progression():
     start = random.randint(1, 10)
     step = random.randint(1, 5)
-    length = random.randint(5, 10)
+    length = 10
 
     progression = [start + step * i for i in range(length)]
     missing_index = random.randint(0, length - 1)
 
-    correct_answer = progression[missing_index]
+    correct_answer = str(progression[missing_index])
     progression[missing_index] = '..' 
     question = ' '.join(map(str, progression))   
 
-    return progression, str(correct_answer)
+    return question, str(correct_answer)
 
 
 def main():
