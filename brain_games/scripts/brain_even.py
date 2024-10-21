126372e25548e00cc54e4ea042190ca160e735bd
barashit <prostopesna74@gmail.com>
2024-10-21 19:12:00 +0300

    Fixed errors-10

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from brain_games.game_general import run_game, setup_game, ROUNDS_TO_WIN
+from brain_games.cli import welcome_user
 from brain_games.games.even import generate_question, GAME_RULES
 
 
 def main():
-    name = setup_game(__import__('brain_games.games.even'))
-    run_game(generate_question, name, rounds=ROUNDS_TO_WIN)
-
+    name = welcome_user()
+    run_game(__import__('brain_games.games.even'), name, rounds=ROUNDS_TO_WIN)
 
 if __name__ == "__main__":
     main()
