fc5dcf39983c07302613b144d610ccafafc9043d
barashit <prostopesna74@gmail.com>
2024-10-27 13:10:07 +0300

    Fixed errors-38

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
 
-from brain_games.cli import welcome_user
+from brain_games.cli import GameCLI
 
 
 def main():
 
-    welcome_user()
-
-
-if __name__ == "__main__":
-    main()
+    cli = GameCLI()
+    cli.welcome_user()
