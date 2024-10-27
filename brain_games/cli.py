1fd108d601308fc4298bd0b5d985a93c685fd64e
barashit <prostopesna74@gmail.com>
2024-10-27 13:19:09 +0300

    Fixed errors-39

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,11 +3,7 @@
 import prompt
 
 class GameCLI:
-    def __init__(self):
-        self.user_name = ""
-
-
-def welcome_user():
-    print("Welcome to the Brain Games!")
-    self.user_name = prompt.string('May I have your name? ')
-    print(f"Hello, {self.user_name}!")
+    def welcome_user(self):
+        print("Welcome to the Brain Games!")
+        user_name = prompt.string('May I have your name? ')
+        print(f"Hello, {user_name}!")
